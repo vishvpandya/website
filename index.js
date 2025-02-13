1661c20d49837ded13c8193c8fcca8307ad73475
vishvpandya <vishwpandya868@gmail.com>
2025-02-13 17:28:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleButton');
     const calendarContainer = document.getElementById('calendarContainer');
+    const backButton = document.getElementById('backButton');
+    const container = document.querySelector('.container');
 
     toggleButton.addEventListener('click', function() {
         calendarContainer.classList.remove('hidden');
-        toggleButton.style.display = 'none';
+        container.style.display = 'none';
+    });
+
+    backButton.addEventListener('click', function() {
+        calendarContainer.classList.add('hidden');
+        container.style.display = 'block';
     });
 });
